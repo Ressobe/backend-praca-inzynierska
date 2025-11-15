e99a30c93e7ec91b8c10f73808eed4d6a34ae938
Bartosz Sobina <bartosz.sobina44@gmail.com>
2025-11-15 16:32:04 +0100

    feat(deployment): add Docker Swarm secrets for database credentials

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 set -e
 
+# Wczytaj sekrety do zmiennych środowiskowych
+if [ -f /run/secrets/tavoo_db_user ]; then
+  export DB_USER=$(cat /run/secrets/tavoo_db_user)
+fi
+
+if [ -f /run/secrets/tavoo_db_pass ]; then
+  export DB_PASS=$(cat /run/secrets/tavoo_db_pass)
+fi
+
+if [ -f /run/secrets/tavoo_db_name ]; then
+  export DB_NAME=$(cat /run/secrets/tavoo_db_name)
+fi
+
 # Wait for the PostgreSQL service to be ready
 echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
 /usr/local/bin/wait-for-it.sh "$DB_HOST:$DB_PORT" --timeout=60 -- echo "PostgreSQL is up - executing command"
