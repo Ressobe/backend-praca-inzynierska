b6e785cdc7a335fda6a415ad99dc4e83aa70e471
Bartosz Sobina <bartosz.sobina44@gmail.com>
2025-11-16 16:33:33 +0100

    test: enable cors for all clients

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Tavoo')
